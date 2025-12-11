1db1ff2eb49cfc4729c1e4765f0d3a6ec00b4b05
samehfathyy <samehfathy1289@gmail.com>
2025-12-12 00:22:46 +0200

    add repo

---
+++ src/main/java/com/example/cinematickets/repos/AuthRepository.java
@@ -0,0 +1,46 @@
+package com.example.cinematickets.repos;
+
+import com.example.cinematickets.models.User;
+
+import java.util.List;
+
+public class AuthRepository {
+    private AuthRepository repo;
+    private AuthRepository(){}
+    public AuthRepository getInstance() {
+        if(repo==null){
+            repo = new AuthRepository();
+        }
+        return repo;
+    }
+    private List<User> users;
+    private User CurrentUser;
+
+    //return true if user registered
+    //false if username is already used by another user
+    boolean registerNewUser(User user){
+        //search for username
+        for (User u : users) {
+            if (u.getUsername().equals(user.getUsername())) {
+                return false;
+            }
+        }
+        user.id=users.size();
+        users.add(user);
+        return true;
+    }
+
+
+    //return true if logged in
+    //false if user or password incorrect
+    boolean login(String username,String password){
+        for (User user : users) {
+            if(user.getUsername().equals(username)&&user.getPassword().equals(password)){
+                CurrentUser=user;
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
